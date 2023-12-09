7138ba352fbdb7237de1d1dbd4aa46d75c352138
XZhao <3536679390@qq.com>
2023-12-09 13:57:01 +0800

    12/9:newnest

---
+++ .vscode/Java/Experience.java
@@ -0,0 +1,32 @@
+package Java;
+
+// import java.util.Scanner;
+
+public class Experience {
+    public static void main(String[] args) {
+        // System.out.println("请输入一个整数:");
+        // try (Scanner bl = new Scanner(System.in)) {
+        // int i = bl.nextInt();
+        // System.out.print("i的值为");
+        // System.out.println(i);
+        // if (i > 10) {
+        // System.out.println("Yes");
+        // } else if (i > 5) {
+        // System.out.println("?");
+        // }
+        // }
+        // int ic = 10;
+        // switch (ic) {
+        // case 1, 2, 3, 4, 5:
+        // System.out.println("?");
+        // break;
+        // case 6, 7, 8, 9, 10:
+        // System.out.println("!");
+        // break;
+        // default:
+        // break;
+        // }
+        int arr[] = { 1, 2, 3 };
+        System.out.println(arr);
+    }
+}
