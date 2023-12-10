d3a8006b43f79cbbe87fb4265d0e29949cc46ada
XZhao <3536679390@qq.com>
2023-12-10 23:25:15 +0800

    小修改，为傅雨晴作样例

--- .vscode/C/work.c
+++ .vscode/C/work.c
@@ -1,12 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-void main()
+// void abc(int a)
+// {
+
+// }
+
+// void main()
+// {
+//     // int arr[5] = {0};
+//     // int *pi = arr;
+//     // printf("%p\n", arr);
+//     // printf("%p\n", &arr);
+//     // printf("%p\n", pi);
+//     int a = 10, b = 20;
+//     abc(a,b);
+//     // return 0;
+// }
+
+#include <stdio.h>
+
+int *compare_and_return(int *a, int *b)
 {
-    int arr[5] = {0};
-    int *pi = arr;
-    printf("%p\n", arr);
-    printf("%p\n", &arr);
-    printf("%p\n", pi);
-    // return 0;
+    if (*a > *b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main()
+{
+    int a = 5, b = 10;
+    int *result = compare_and_return(&a, &b);
+    printf("The larger value is %d\n", *result);
+    return 0;
 }
